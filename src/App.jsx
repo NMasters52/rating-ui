c881a7b1a7a31cec99aa710497a162f8015c5f1d
nmasters <nmasters52@gmail.com>
2025-07-24 14:15:45 -0400

    basic rating set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
 
 function App() {
-  
+  const [rating, setRating] = useState(0);
+
+  const stars = Array.from({length: 5}, (_, i) => i + 1);
+
+  const ratingClick = (star) => {
+    setRating(star);
+  }
+
+  const starActive = (star) => star <= rating ? 'active' : '';
+
+
   return (
     <>
-      Hello App
+     <div className="rating-container">
+      <div className="stars">
+        {stars.map((star, index) => (
+           <span 
+            className={`star ${starActive(star)}`}
+            key={star}
+            onClick={() => ratingClick(star)}
+          >
+            &#9733;
+          </span>)
+          )}
+      </div>
+     <h2>Rate your experience</h2>
+     </div>
     </>
   )
 }
